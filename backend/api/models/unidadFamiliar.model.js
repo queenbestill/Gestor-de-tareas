091de7ec56448bda567e409d99f72c44bf136359
queenbestill <inglescanarias@gmail.com>
2024-06-18 16:11:08 +0100

    crud familiar

--- backend/api/models/unidadFamiliar.model.js
+++ backend/api/models/unidadFamiliar.model.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require("sequelize");
-const sequelize = require("../../db"); 
+const sequelize = require("../../db");
 
 const UnidadFamiliar = sequelize.define(
   "UnidadFamiliar",
