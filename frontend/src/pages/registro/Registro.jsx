35d342c37aacbec5bc5abf266baa616e940f4f13
dcarrete <dcarrete@me.com>
2024-06-20 16:56:35 +0100

    registro done

--- frontend/src/pages/registro/Registro.jsx
+++ frontend/src/pages/registro/Registro.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-//import { signup } from '../services/auth.service'
+import { signup } from '../../services/auth.service'
 import "./Registro.css";
 import { useNavigate } from "react-router-dom";
 
