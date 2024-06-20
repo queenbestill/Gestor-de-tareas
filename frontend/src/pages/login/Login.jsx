c521eac6a10eabeba291cd8a7c991a59ea0a19c8
castiblancoely <castiblancoely@gmail.com>
2024-06-20 14:51:08 +0100

    Create Login in Frontend with response

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useState } from "react";
+import '../login/Login.css'
+import { login } from "../../services/auth.service";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("Email:", email);
+    console.log("Password:", password);
+    const response = await login(email, password)
+
+    localStorage.setItem('token', response.token)
+    console.log(response)
+
+  };
+
   return (
-    <div>Login</div>
-  )
+    <header className="container"><div className="izquierda">
+        <img src="/frontend/public/ilustracion3.jpg" alt="ilustration" />
+      </div>
+      <div>
+        <div className="login-container">
+          <h1>Login</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="input-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div className="input-group">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <button type="submit">Login</button>
+          </form>
+        </div>
+      </div>
+    </header>
+  );
 }
 
-export default Login
+export default Login;
