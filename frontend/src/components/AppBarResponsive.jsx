8069801a65afab86bcbfd7a905e8fd45d81fc0ea
queenbestill <inglescanarias@gmail.com>
2024-06-26 11:16:27 +0100

    cambié los boones del navbar

--- frontend/src/components/AppBarResponsive.jsx
+++ frontend/src/components/AppBarResponsive.jsx
@@ -1,14 +1,26 @@
-import React, { useState } from 'react'
-import AppBar from '@mui/material/AppBar';
-import { Avatar, Box, Button, Container, IconButton, Menu, MenuItem, Toolbar, Tooltip, Typography } from '@mui/material';
+import React, { useState, useEffect } from "react";
+import AppBar from "@mui/material/AppBar";
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Tooltip,
+  Typography,
+} from "@mui/material";
 import logo from "../../public/logo nav.png";
-import MenuIcon from '@mui/icons-material/Menu';
-import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew';
-import AddCircleIcon from '@mui/icons-material/AddCircle';
-import { useNavigate } from 'react-router-dom';
+import MenuIcon from "@mui/icons-material/Menu";
+import PowerSettingsNewIcon from "@mui/icons-material/PowerSettingsNew";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
-const pages = ['Products', 'Pricing', 'Blog'];
+const pages = ["Crear tarea", "Ver todas las tareas", "Ver mis tareas"];
 
 const Header = () => {
   return (
@@ -22,35 +34,47 @@ const Header = () => {
 };
 
 function AppBarResponsive() {
-    const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
-  
-    const handleOpenNavMenu = (event) => {
-      setAnchorElNav(event.currentTarget);
-    };
-   
-  
-    const handleCloseNavMenu = () => {
-      setAnchorElNav(null);
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        const response = await axios.get("/api/user", {
+          headers: {
+            Authorization: token,
+          },
+        });
+        setUser(response.data);
+      } catch (error) {
+        console.error("Error al obtener los datos del usuario:", error.message);
+      }
     };
 
-    const isLoggedIn = Boolean(localStorage.getItem('token'));
-  
-    return (
-      <AppBar position="static">
-        <Container maxWidth="xl">
-          <Toolbar disableGutters>
-            <img
-              src={logo}
-              alt="Logo"
-              style={{ margin:20}}
-            />
+    fetchUserData();
+  }, []);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const isLoggedIn = Boolean(localStorage.getItem("token"));
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+            <img src={logo} alt="Logo" style={{ margin: 20 }} />
             <Typography
               variant="h6"
               noWrap
-              component="a"
-              href="#app-bar-with-responsive-menu"
               sx={{
                 mr: 2,
                 display: { xs: "none", md: "flex" },
@@ -63,122 +87,118 @@ function AppBarResponsive() {
             >
               EasyTask
             </Typography>
+          </Link>
 
-            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenNavMenu}
-                color="inherit"
-              >
-                <MenuIcon />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorElNav}
-                anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "left",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "left",
-                }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
-                sx={{
-                  display: { xs: "block", md: "none" },
-                }}
-              >
-                {pages.map((page) => (
-                  <MenuItem key={page} onClick={handleCloseNavMenu}>
-                    <Typography textAlign="center">{page}</Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
-            </Box>
-
-
-            <Typography
-              variant="h5"
-              noWrap
-              component="a"
-              href="#app-bar-with-responsive-menu"
-              sx={{
-                mr: 2,
-                display: { xs: "flex", md: "none" },
-                flexGrow: 1,
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "inherit",
-                textDecoration: "none",
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
               }}
-            ></Typography>
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
 
-            {isLoggedIn ? (
-              <>
-                <Box
-                  sx={{
-                    flexGrow: 1,
-                    display: {
-                      xs: "none",
-                      md: "flex",
-                      alignItems: "center",
-                      justifyContent: "center",
-                      gap: "20px",
-                    },
-                  }}
-                >
-                  <AddCircleIcon sx={{ height: "50px", width: "40px" }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          ></Typography>
 
-                  <Typography> Usuario</Typography>
-                </Box>
-
-                <PowerSettingsNewIcon
-                  sx={{
-                    color: "red",
-                    width: "50px",
-                    ":hover": { bgcolor: "red-500" },
-                  }}
-                />
-              </>
-            ) : (
+          {isLoggedIn ? (
+            <>
               <Box
                 sx={{
-                  display: "flex",
-                  justifyContent: "space-between",
                   flexGrow: 1,
+                  display: {
+                    xs: "none",
+                    md: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    gap: "20px",
+                  },
                 }}
               >
-                <Box sx={{ flexGrow: 1 }} />
-                <Button
-                  key={"login"}
-                  onClick={function () {
-                    navigate("/login");
-                  }}
-                  sx={{
-                    my: 2,
-                    alignSelf: "end",
-                    width: "200px",
-                    bgcolor: "white",
-                    color: "black",
-                    border: "black",
-                    ":hover": { bgcolor: "yellow" },
-                  }}
-                >
-                  Iniciar sesión
-                </Button>
+                <AddCircleIcon sx={{ height: "50px", width: "40px" }} />
+                {user && <Typography>{user.name}</Typography>}
               </Box>
-            )}
-          </Toolbar>
-        </Container>
-      </AppBar>
-    );
 
+              <PowerSettingsNewIcon
+                sx={{
+                  color: "red",
+                  width: "50px",
+                  ":hover": { bgcolor: "red-500" },
+                }}
+              />
+            </>
+          ) : (
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                flexGrow: 1,
+              }}
+            >
+              <Box sx={{ flexGrow: 1 }} />
+              <Button
+                key={"login"}
+                onClick={() => navigate("/login")}
+                sx={{
+                  my: 2,
+                  alignSelf: "end",
+                  width: "200px",
+                  bgcolor: "white",
+                  color: "black",
+                  border: "black",
+                  ":hover": { bgcolor: "yellow" },
+                }}
+              >
+                Iniciar sesión
+              </Button>
+            </Box>
+          )}
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 }
 
-export default AppBarResponsive
+export default AppBarResponsive;
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -19,7 +19,7 @@ function Login() {
     localStorage.setItem('token', response.token)
     setUser(response.user)
     
-    navigate('/')
+    navigate("/tareas");
     console.log(user)
 
   };
